a4ca4ec1043e4a8d6efe4c28a66a1aa1d1be7c75
Emresaridogan <emresaridogan@outlook.com>
2023-01-22 19:20:31 +0300

    models added

---
+++ lib/features/data/models/user/auth_model.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../../domain/entities/auth/auth_entity.dart';
+
+class AuthenticationModel extends AuthenticationEntity {
+  const AuthenticationModel({required super.userId});
+
+  factory AuthenticationModel.fromFirebaseUser(User user) {
+    return AuthenticationModel(userId: user.uid);
+  }
+}
